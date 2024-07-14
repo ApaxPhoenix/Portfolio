82a5e9d5c2472cd8c7e4c13cbdd0e09d43604bda
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-07-14 16:38:35 -0400

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Balancer from "react-wrap-balancer";
 import {ArrowRight} from "lucide-react";
-import {Section, Container} from "@/components/craft";
-import {Button} from "@/components/ui/button";
+import {Section, Container} from "./craft";
+import {Button} from ./ui/button";
 
 const Hero = () => {
     return (
