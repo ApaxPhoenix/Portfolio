2716194bc8847757d72dc4f9b4657719954ffe22
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-07-14 16:39:22 -0400

    Update faq.tsx

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -5,7 +5,7 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "../ui/accordion";
+} from "./ui/accordion";
 import {Section, Container} from "./craft";
 
 type FaqItem = {
