27aa8c36493e7f280bc5ab329bae9b4359c89386
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-07-17 20:38:14 -0400

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,6 @@ export default function App({Component, pageProps}: AppProps) {
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange
         >
             <Component {...pageProps} />
         </ThemeProvider>
