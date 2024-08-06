4ee9aae70fba5dd1ca0c379c3489c7cbfabe4f32
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-08-05 23:48:13 -0400

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,22 +2,21 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 import Cors from 'cors';
 
-interface Package {
-    name: string;
-    subject: string;
-    text: string;
-}
+const allowedOrigins = ['https://apaxphoenix.github.io', 'http://localhost:3000'];
 
 const cors = Cors({
-    origin: ['hhtps://apaxphoenix.github.io', 'http://localhost'],
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['POST'],
 });
 
-function runMiddleware(
-    req: NextApiRequest,
-    res: NextApiResponse,
-    fn: Function
-) {
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
     return new Promise((resolve, reject) => {
         fn(req, res, (result: any) => {
             if (result instanceof Error) {
@@ -38,10 +37,9 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export default async function handler(
-    request: NextApiRequest,
-    res: NextApiResponse<{ message: string }>
-) {
+export default async function handler(request: NextApiRequest, res: NextApiResponse<{ message: string }>) {
+    console.log('Request method:', request.method);
+
     await runMiddleware(request, res, cors);
 
     if (request.method === 'POST') {
@@ -70,6 +68,7 @@ export default async function handler(
             }
         }
     } else {
+        console.log(`Method ${request.method} Not Allowed`);
         res.setHeader('Allow', ['POST']);
         res.status(405).json({ message: `Method ${request.method} Not Allowed` });
     }
